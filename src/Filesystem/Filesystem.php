b2e1f87134541efd3e44565935666c017c4b24bb
Loïc Sapone <loic@sapone.fr>
2024-04-24 12:03:09 +0200

    Delegate provider responsibility to a new storage model

--- src/Filesystem/Filesystem.php
+++ src/Filesystem/Filesystem.php
@@ -13,52 +13,50 @@ declare(strict_types=1);
 
 namespace Mezcalito\FileManagerBundle\Filesystem;
 
-use Mezcalito\FileManagerBundle\Provider\ProviderInterface;
-
 readonly class Filesystem implements FilesystemInterface
 {
     public function __construct(
-        private ProviderInterface $provider,
+        private StorageInterface $storage,
     ) {
     }
 
     public function read(string $id): string
     {
-        return $this->provider->read($id);
+        return $this->storage->getProvider()->read($id);
     }
 
     public function write(string $id, string $contents): void
     {
-        $this->provider->write($id, $contents);
+        $this->storage->getProvider()->write($id, $contents);
     }
 
     public function delete(string $id): void
     {
-        $this->provider->delete($id);
+        $this->storage->getProvider()->delete($id);
     }
 
     public function move(string $id, string $source, string $destination): void
     {
-        $this->provider->copy($id, $source, $destination);
+        $this->storage->getProvider()->copy($id, $source, $destination);
     }
 
     public function copy(string $id, string $source, string $destination): void
     {
-        $this->provider->copy($id, $source, $destination);
+        $this->storage->getProvider()->copy($id, $source, $destination);
     }
 
     public function listDirectory(string $id, bool $recursive = false): \Generator
     {
-        return $this->provider->listDirectory($id);
+        return $this->storage->getProvider()->listDirectory($id);
     }
 
     public function createDirectory(string $id, int $permissions = 0o777): void
     {
-        $this->provider->createDirectory($id, $permissions);
+        $this->storage->getProvider()->createDirectory($id, $permissions);
     }
 
     public function deleteDirectory(string $id): void
     {
-        $this->provider->deleteDirectory($id);
+        $this->storage->getProvider()->deleteDirectory($id);
     }
 }
